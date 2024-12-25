b61b5b13b171e6e65ad662615152ad2f6ccb6d20
soumyesh panda <soumyesh.panda@merklecxm.com>
2024-12-25 13:44:54 +0530

    EP4-first-created static structure for restaurant

--- App.js
+++ App.js
@@ -1,23 +1,66 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-//jsx or react element
-const heading = <h1>Welcome to JSX in React</h1>
-
-// fucntional component
-
-const Heading2 = () => {return <h2>Welcome to JSX in React</h2>}
-
-// component composition - calling another component with in a component
-
-const HeadingComponent = () => {
+const Header = () => {
     return (
-        <div class="wrapper">
-            <Heading2/>
-            <h1>This is functional component</h1>
+        <div className="header d-flex">
+        <div className="logoContainer">
+            <img className="logo" src = "https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"></img>
+        </div>
+        <div className="nav-items d-flex">
+            <ul className="d-flex">
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="d-flex w-100 mtb-10 card">
+            <img 
+                className="res-img"
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/21/dbe91f1f-b400-4f4b-a78d-c6b99bdc61c5_912754.JPG"
+            />
+            <div className="card-items">Meghna Foods</div>
+            <div className="card-items">Biriyani, North Indian</div>
+            <div className="card-items">4.4 stars</div>
+            <div className="card-items">38 minutes</div>
+        </div>
+    )
+}
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search-wrapper">
+                <input type="text" className="search-box mr-10" value="Search"></input>
+            </div>
+            <div className="res-container res-card-wrapper d-flex">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
         </div>
     )
 }
 
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
